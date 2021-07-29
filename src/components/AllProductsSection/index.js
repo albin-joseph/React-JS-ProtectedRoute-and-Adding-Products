9b8a78c3687312140916f94a18fabf4f69a67ef1
umamaheshkalla <maheshuma916@gmail.com>
2021-07-29 09:19:02 +0000

    React-JS-ProtectedRoute-and-Adding-Products

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+
+import Cookies from 'js-cookie'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    console.log(response)
+    if (response.ok === true) {
+      const data = await response.json()
+      console.log(data)
+      const updatedData = data.products.map(eachProduct => ({
+        brand: eachProduct.brand,
+        id: eachProduct.id,
+        imageUrl: eachProduct.image_url,
+        price: eachProduct.price,
+        title: eachProduct.title,
+        rating: eachProduct.rating,
+      }))
+      this.setState({productsList: updatedData})
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
